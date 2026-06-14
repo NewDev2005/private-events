6e43594226b082bf718ee2a22dec66d3cdda8c40
krypto <clayjeansen3@gmail.com>
2026-06-14 09:51:51 +0530

    add email and username column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_12_174714) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_14_025919) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "date"
     t.string "location"
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end

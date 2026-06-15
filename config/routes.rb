8efe3c9f5a23f77a6aba7a3f64152aae691d7a25
krypto <clayjeansen3@gmail.com>
2026-06-15 22:19:56 +0530

    add index action for events controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  resources :events
+  resources :events, only: [ :new, :create, :index ]
   root "events#index"
 end

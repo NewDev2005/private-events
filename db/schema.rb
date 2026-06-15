6ffdcc13f9649b4fc21bb590fec68d346723208a
krypto <clayjeansen3@gmail.com>
2026-06-15 22:24:34 +0530

    update schema with foreign key creator_id in events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_14_025919) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_14_101549) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.integer "creator_id", null: false
     t.date "date"
     t.string "location"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_025919) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end

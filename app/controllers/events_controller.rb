f528684a53180858669b34ecdb681b0f4279f309
krypto <clayjeansen3@gmail.com>
2026-06-15 22:22:12 +0530

    allow user to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,26 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.events.build(allowed_post_params)
+    if @event.save!
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_post_params
+    params.expect(event: [ :name, :location, :date ])
   end
 end
